43143b405783ee73ec0d6d4c78bb2bb29c39e388
Jonas Gamburg <yonatan.gamburg@envidual.com>
2022-10-21 09:57:22 +0200

    Initial commit

---
+++ Cloudy_DribbbleConcept/Cloudy_DribbbleConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cloudy_DribbbleConceptApp.swift
+//  Cloudy_DribbbleConcept
+//
+//  Created by Jonas Gamburg on 21.10.22.
+//
+
+import SwiftUI
+
+@main
+struct Cloudy_DribbbleConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
